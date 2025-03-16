0f8135e538498125aa54be92474c95de2bea12fc
Stefan Vladescu <stefan.vladescu2012@gmail.com>
2025-03-16 12:36:49 +0200

    Added logic for reading the dom in the extension

--- web extension/popups/index/index.js
+++ web extension/popups/index/index.js
@@ -1,1 +1,33 @@
-console.log("Over here!")
+window.onload = ()=>{
+    document.getElementById('btnStart').addEventListener('click', getTabHTML({ active: true, currentWindow: true }))
+}
+
+function getTabHTML(filter){
+    chrome.tabs.query(filter).then(function (tabs) {
+        var activeTab = tabs[0];
+        var activeTabId = activeTab.id;
+
+        return chrome.scripting.executeScript({
+            target: { tabId: activeTabId },
+            func: DOMtoString,
+            args: ['body']  
+        });
+
+    }).then(function (results) {
+        console.log(results[0].result) 
+    }).catch(function (error) {
+        console.log('There was an error injecting script : \n' + error.message);
+    });
+
+
+}
+
+function DOMtoString(selector) {
+    if (selector) {
+        selector = document.querySelector(selector);
+        if (!selector) return "ERROR: querySelector failed to find node"
+    } else {
+        selector = document.documentElement;
+    }
+    return selector.outerHTML;
+}
